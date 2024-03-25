e26f3be18a57728353f278d7986ab87c654978aa
manon <s36123@stagiaire.aelion.fr>
2024-03-25 13:22:48 +0100

    Load components in router outlet placeholder - Add some routes in  router definition - Update app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    redirectTo: 'home',
+    pathMatch: 'full' 
+  },
+  {
+    path:"home",
+    component: HomeComponent,
+    pathMatch:'full'
+  },
+  //! Pour rediriger vers home qui remplace nimporte quel redirection, si l'user essaye de faire un / qqchose
+  {
+    path:'**',
+    redirectTo:'home',
+    pathMatch:'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
