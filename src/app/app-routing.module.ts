184540fab6d8624595284f86d13369dcf30d87df
manon <s36123@stagiaire.aelion.fr>
2024-03-29 11:26:19 +0100

    US 1 : - Dans app-routing : création de la route vers Accueil POE - Dans home component général création du bouton Poe-home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { InternFormComponent } from './home/intern-form/intern-form.component';
+import { PoeHomeComponent } from './poe/poe-home/poe-home.component';
 
 const routes: Routes = [
   {
@@ -18,6 +19,12 @@ const routes: Routes = [
     path:"intern-add",
     component: InternFormComponent,
     pathMatch:"full"
+  },
+
+  {
+    path:"poe-home",
+    component: PoeHomeComponent,
+    pathMatch: "full"
   },
   //! Pour rediriger vers home qui remplace nimporte quel redirection, si l'user essaye de faire un / qqchose
   {
